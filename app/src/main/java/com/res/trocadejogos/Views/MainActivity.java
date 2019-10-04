5da242c6c0b27d40983e2d9002fd867310f099df
samututu <samututu@hotmail.com>
2019-10-03 22:44:45 -0300

    Ação botão Login

--- app/src/main/java/com/res/trocadejogos/Views/MainActivity.java
+++ app/src/main/java/com/res/trocadejogos/Views/MainActivity.java
@@ -5,26 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.res.trocadejogos.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView cad;
+    private TextView registrar;
+    private Button entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-          cad = findViewById(R.id.textRegistrarButton);
-
-        cad.setOnClickListener(new View.OnClickListener() {
+        registrar = findViewById(R.id.textRegistrarButton);
+        registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent it = new Intent(MainActivity.this, CadastroUsuario.class);
+                startActivity(it);
+
+            }
+        });
+
+        entrar = findViewById(R.id.buttonEntrar);
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent it = new Intent(MainActivity.this, Biblioteca.class);
                 startActivity(it);
 
             }
