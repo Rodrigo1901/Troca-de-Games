16b7985ec558762d4ffcc423207e547d7a535812
samututu <samututu@hotmail.com>
2019-10-07 23:18:46 -0300

    Colocando logo do sistema e criando classe Conversas v2

--- app/src/main/java/com/res/trocadejogos/Views/Conversas.java
+++ app/src/main/java/com/res/trocadejogos/Views/Conversas.java
@@ -1,9 +1,11 @@
-package com.res.trocadejogos;
+package com.res.trocadejogos.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
+import com.res.trocadejogos.R;
 
 public class Conversas extends AppCompatActivity {
 
