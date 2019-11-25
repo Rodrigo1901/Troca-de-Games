2e96fc4c94ff0bc29a73209dc9e04d89ecf2412e
samututu <samututu@hotmail.com>
2019-11-24 21:34:47 -0300

    Novo mapa v6

--- app/src/main/java/com/res/trocadejogos/Adapter/CustomInfoWindowAdapter.java
+++ app/src/main/java/com/res/trocadejogos/Adapter/CustomInfoWindowAdapter.java
@@ -1,7 +1,10 @@
 package com.res.trocadejogos.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
@@ -9,23 +12,41 @@ import com.res.trocadejogos.R;
 
 public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
+    private final View mWindow;
+    private Context mContext;
 
-
-
-    private void rendowWindowText(Marker marker,View view){
-
-        Button conversar = (Button) view.findViewById(R.id.conversar);
-
+    public CustomInfoWindowAdapter(Context context) {
+        mContext = context;
+        mWindow = LayoutInflater.from(context).inflate(R.layout.custom_info_window, null);
     }
 
+    private void rendowWindowText(Marker marker, View view) {
+
+        Button conversar = (Button) view.findViewById(R.id.conversar);
+        String title = marker.getTitle();
+        TextView tvTitle = view.findViewById(R.id.nomeMap);
+
+        if (!title.equals("")) {
+            tvTitle.setText(title);
+        }
+
+        String snippet = marker.getSnippet();
+        TextView tvSnippet = view.findViewById(R.id.negoMap);
+
+        if (!snippet.equals("")) {
+            tvSnippet.setText(snippet);
+        }
+    }
 
     @Override
     public View getInfoWindow(Marker marker) {
-        return null;
+        rendowWindowText(marker, mWindow);
+        return mWindow;
     }
 
     @Override
     public View getInfoContents(Marker marker) {
-        return null;
+        rendowWindowText(marker, mWindow);
+        return mWindow;
     }
 }
