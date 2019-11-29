4d6a4c9bcd5d69be89f6ba513ed5d19a2a462cbc
samututu <samututu@hotmail.com>
2019-11-28 23:15:40 -0300

    Chat v2

--- app/src/main/java/com/res/trocadejogos/Views/Chat.java
+++ app/src/main/java/com/res/trocadejogos/Views/Chat.java
@@ -12,6 +12,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -23,12 +24,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.StorageReference;
 import com.res.trocadejogos.Adapter.MensagensAdapter;
 import com.res.trocadejogos.Classes.Mensagem;
 import com.res.trocadejogos.Classes.Usuario;
+import com.res.trocadejogos.Config.Base64Custom;
 import com.res.trocadejogos.Config.ConfigFirebase;
+import com.res.trocadejogos.Config.FirebaseUser;
 import com.res.trocadejogos.R;
 
 import java.util.ArrayList;
@@ -40,15 +46,18 @@ public class Chat extends AppCompatActivity {
 
     private long backPressedTime;
     private Toast backToast;
-    private String id;
-    private String nome;
-    private DatabaseReference databaseReference;
+    private DatabaseReference mensagensRef;
+    private ChildEventListener childEventListenerMensagens;
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
+    private String nomeUsuarioDestinatario;
+    private DatabaseReference database;
     private StorageReference storageReference;
     private EditText editMensagem;
     private RecyclerView recyclerMensagens;
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
-    private Usuario usuarioDestinatario;
+    //private Usuario usuarioDestinatario;
     private MensagensAdapter adapter;
     private List<Mensagem> mensagens = new ArrayList<>();
 
@@ -58,9 +67,10 @@ public class Chat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        //Recuperar dados do usuario destinatario
         Intent it = getIntent();
-        id = it.getStringExtra("id");
-        nome = it.getStringExtra("nome");
+        idUsuarioDestinatario = it.getStringExtra("id");
+        nomeUsuarioDestinatario = it.getStringExtra("nome");
 
         //Configurando toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -68,9 +78,8 @@ public class Chat extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        databaseReference = ConfigFirebase.getFirebaseDatabase();
+        database = ConfigFirebase.getFirebaseDatabase();
         storageReference = ConfigFirebase.getFirebaseStorage();
-
 
         //Configurações iniciais
         textViewNome = findViewById(R.id.textViewNomeChat);
@@ -78,9 +87,22 @@ public class Chat extends AppCompatActivity {
         editMensagem = findViewById(R.id.editMensagem);
         recyclerMensagens = findViewById(R.id.recyclerMensagens);
 
-        textViewNome.setText(nome);
+        //Recuperar dados do usuario remetente
+        idUsuarioRemetente = FirebaseUser.getIdentificadorUsuario();
 
-        storageReference.child("imagens").child("perfil").child(id + ".jpeg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+        /*Recuperar dados do usuario destinatario (Aula 308)
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null) {
+
+            usuarioDestinatario = (Usuario) bundle.getSerializable("chatContato");
+            textViewNome.setText(usuarioDestinatario.getNome());
+        }*/
+
+        //Atualizar nome e foto do usuario destinatario na tela de chat
+        textViewNome.setText(nomeUsuarioDestinatario);
+
+        storageReference.child("imagens").child("perfil").child(idUsuarioDestinatario + ".jpeg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
                 Glide.with(Chat.this).load(uri).into(circleImageViewFoto);
@@ -92,17 +114,6 @@ public class Chat extends AppCompatActivity {
             }
         });
 
-
-        //TODO: Continuar após ter mapa pronto. Pois precisamos ter o usuario destinatario (Aula 308)
-        /*Recuperar dados do usuario destinatario
-        Bundle bundle = getIntent().getExtras();
-
-        if (bundle != null) {
-
-            usuarioDestinatario = (Usuario) bundle.getSerializable("chatContato");
-            textViewNome.setText(usuarioDestinatario.getNome());
-        }*/
-
         //Configuração adapter
         adapter = new MensagensAdapter(mensagens, getApplicationContext());
 
@@ -111,6 +122,8 @@ public class Chat extends AppCompatActivity {
         recyclerMensagens.setLayoutManager(layoutManager);
         recyclerMensagens.setHasFixedSize(true);
         recyclerMensagens.setAdapter(adapter);
+
+        mensagensRef = database.child("mensagens").child(idUsuarioRemetente).child(idUsuarioDestinatario);
     }
 
     @Override
@@ -135,11 +148,72 @@ public class Chat extends AppCompatActivity {
         if (!textoMensagem.isEmpty()) {
 
             Mensagem mensagem = new Mensagem();
-            //mensagem.setIdUsuario();
+            mensagem.setIdUsuario(idUsuarioRemetente);
             mensagem.setMensagem(textoMensagem);
+
+            //Salvar mensagem para o remetente
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+
+            //Salvar mensagem para o destinatario
+            salvarMensagem(idUsuarioDestinatario, idUsuarioRemetente, mensagem);
 
         } else {
             Toast.makeText(Chat.this, "Digite uma mensagem para enviar!", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg) {
+
+        DatabaseReference database = ConfigFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+
+        mensagemRef.child(idRemetente).child(idDestinatario).push().setValue(msg);
+
+        //Limpar texto
+        editMensagem.setText("");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarMensagens();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mensagensRef.removeEventListener(childEventListenerMensagens);
+    }
+
+    private void recuperarMensagens() {
+
+        childEventListenerMensagens = mensagensRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Mensagem mensagem = dataSnapshot.getValue(Mensagem.class);
+                mensagens.add(mensagem);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
